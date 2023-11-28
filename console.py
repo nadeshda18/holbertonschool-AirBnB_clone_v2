2dc17f662f36cdf3d1bc4784705fc5bbf597bf38
SMCastr <mariesweetycastro@gmail.com>
2023-11-28 15:28:50 -0400

    fix libraries

--- console.py
+++ console.py
@@ -2,6 +2,9 @@
 """Console Module"""
 import cmd
 import sys
+import unittest
+import os
+import subprocess
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
