06c914e1bf6c29dcac8246fb08e2e33689d1ba9e
SMCastr <mariesweetycastro@gmail.com>
2023-11-28 13:44:41 -0400

    update structure add def

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import subprocess
 import unittest
 from io import StringIO
@@ -8,37 +10,48 @@ from models.state import State
 from models.place import Place
 
 class TestConsole(unittest.TestCase):
+    """Test cases for the HBNBCommand console."""
 
     @classmethod
     def setUpClass(cls):
         """Setup for the test cases."""
         cls.console = HBNBCommand()
-        cls.obj_list = []
-        cls.obj_list.append(BaseModel())
-        cls.obj_list.append(State(name="California"))
-        cls.obj_list.append(Place(city_id="0001", user_id="0001", name="My_little_house", number_rooms=4, number_bathrooms=2, max_guest=10, price_by_night=300, latitude=37.773972, longitude=-122.431297))
+        cls.obj_list = [
+            BaseModel(),
+            State(name="California"),
+            Place(city_id="0001", user_id="0001", name="My_little_house", number_rooms=4, number_bathrooms=2, max_guest=10, price_by_night=300, latitude=37.773972, longitude=-122.431297)
+        ]
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_command(self, mock_stdout):
+        """Test the create command without parameters."""
         with patch('sys.stdin', StringIO("create BaseModel\nall\n")):
             self.console.cmdloop()
             output = mock_stdout.getvalue().strip()
-            self.assertIn(str(self.obj_list[0]), output)
+            self.assertIn(str(self.obj_list[0]), output, "Failed in the create command test without parameters.")
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_command_with_params(self, mock_stdout):
+        """Test the create command with parameters."""
         with patch('sys.stdin', StringIO("create State name=\"California\"\nall State\n")):
             self.console.cmdloop()
             output = mock_stdout.getvalue().strip()
-            self.assertIn("California", output)
+            self.assertIn("California", output, "Failed in the create command test with parameters.")
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_command_with_complex_params(self, mock_stdout):
+        """Test the create command with complex parameters."""
         with patch('sys.stdin', StringIO("create Place city_id=\"0001\" user_id=\"0001\" name=\"My_little_house\" number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300 latitude=37.773972 longitude=-122.431297\nall Place\n")):
             self.console.cmdloop()
             output = mock_stdout.getvalue().strip()
-            self.assertIn("My little house", output)
-            self.assertIn("37.773972", output)
+            self.assertIn("My little house", output, "Failed in the create command test with complex parameters.")
+            self.assertIn("37.773972", output, "Failed in the create command test with complex parameters.")
+
+    def test_quit_command(self):
+        """Test the quit command."""
+        with patch('sys.stdout', new_callable=StringIO):
+            with self.assertRaises(SystemExit):
+                self.console.onecmd("quit")
 
 if __name__ == '__main__':
     unittest.main()
