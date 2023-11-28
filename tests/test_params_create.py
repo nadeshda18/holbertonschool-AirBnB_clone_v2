3e57b4323953a0c1d5e8c2b4f224602d2c58c42e
SMCastr <mariesweetycastro@gmail.com>
2023-11-28 13:44:53 -0400

    fix libraries

--- tests/test_params_create.py
+++ tests/test_params_create.py
@@ -1,31 +1,36 @@
-# test_params_create
+#!/usr/bin/env python3
+import subprocess
+import unittest
 
-def create_state_commands():
-    # Define commands to create State objects
-    return [
-        'create State name="California"',  # Create a State object with the name "California"
-        'create State name="Arizona"',      # Create another State object with the name "Arizona"
-        'all State'                          # Display all State objects
+def test_params_create():
+    # Define commands to create State and Place objects
+    state_commands = [
+        'create State name="California"',
+        'create State name="Arizona"',
+        'all State'
     ]
 
-def create_place_commands():
-    # Define commands to create Place objects
-    return [
-        'create Place city_id="0001" user_id="0001" name="My_little_house" number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300 latitude=37.773972 longitude=-122.431297',  # Create a Place object with specified parameters
-        'all Place'                          # Display all Place objects
+    place_commands = [
+        'create Place city_id="0001" user_id="0001" name="My_little_house" number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300 latitude=37.773972 longitude=-122.431297',
+        'all Place'
     ]
+
+    # Concatenate all commands
+    all_commands = '\n'.join(state_commands + place_commands)
+
+    # Run the commands using subprocess
+    result = subprocess.run(['echo', all_commands, '|', './console.py'], text=True, capture_output=True)
+
+    # Check if the output contains the expected results
+    assert "California" in result.stdout
+    assert "Arizona" in result.stdout
+    assert "My little house" in result.stdout
+    assert "number_rooms: 4" in result.stdout
+    assert "number_bathrooms: 2" in result.stdout
+    assert "max_guest: 10" in result.stdout
+    assert "price_by_night: 300" in result.stdout
+    assert "latitude: 37.773972" in result.stdout
+    assert "longitude: -122.431297" in result.stdout
 
 if __name__ == "__main__":
-    # Generate commands for State and Place objects
-    state_commands = create_state_commands()
-    place_commands = create_place_commands()
-
-    # Open the file for writing
-    with open('test_params_create | ./console.py', 'w') as file:
-        # Write State commands to the file
-        for command in state_commands:
-            file.write(command + '\n')
-
-        # Write Place commands to the file
-        for command in place_commands:
-            file.write(command + '\n')
+    test_params_create()
