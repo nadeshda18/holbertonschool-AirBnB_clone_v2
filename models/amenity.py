ff144996c45f1e2edc796172db9ebd7d75e5132e
SMCastr <mariesweetycastro@gmail.com>
2023-11-30 19:46:31 -0400

    fix models update

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import getenv
+
 
 class Amenity(BaseModel):
-    """This class defines a Amenity class for AirBnB"""
     name = ""
