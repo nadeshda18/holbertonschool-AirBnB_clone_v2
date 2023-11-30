071452463c06396e5445985c436a6739ee3bf9cc
SMCastr <mariesweetycastro@gmail.com>
2023-11-30 14:10:10 -0400

    update user table

--- models/user.py
+++ models/user.py
@@ -1,13 +1,4 @@
-#!/usr/bin/python3
-""" User Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
