1edb9eb779aa57d1f538354a94b2f8821635777a
SMCastr <mariesweetycastro@gmail.com>
2023-11-30 14:18:21 -0400

    update import

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, DateTime, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime
-import uuid
-import models
-import os
-from models.base_model import BaseModel, Base
-from models.city import City
-from models.state import State
-from models.user import User
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 
 
 class Amenity(BaseModel):
