ef81da2cddc16058e4965c0a01d0428243bce1de
SMCastr <mariesweetycastro@gmail.com>
2023-11-30 14:35:49 -0400

    done __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,7 @@
-from os import getenv
+#!/usr/bin/python3
+""" Init module for HBNB project """
+from models.engine.file_storage import FileStorage
 
-HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
-if HBNB_TYPE_STORAGE == 'db':
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-
+storage = FileStorage()
 storage.reload()
