0236c23f883256fe541444f855b621affd4ce0af
SMCastr <mariesweetycastro@gmail.com>
2023-11-30 12:47:04 -0400

    update imports & do create &update

--- console.py
+++ console.py
@@ -14,6 +14,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from models.base_model import BaseModel, Base
 
 
 class HBNBCommand(cmd.Cmd):
