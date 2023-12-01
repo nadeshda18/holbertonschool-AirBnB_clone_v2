36eb76256db8a4a0bd35656c3b4a8e576523e504
SMCastr <mariesweetycastro@gmail.com>
2023-12-01 15:21:30 -0400

    Merge fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-<<<<<<< HEAD
-from models.engine.file_storage import FileStorage
-from os import getenv
-from models.engine.db_storage import DBStorage
-
-
-HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
-
-if HBNB_TYPE_STORAGE == 'db':
-    storage = DBStorage()
-    storage.reload()
-
-else:
-=======
 from os import getenv
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
@@ -22,6 +8,5 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
     storage.reload()
 else:
     from models.engine.file_storage import FileStorage
->>>>>>> master
     storage = FileStorage()
     storage.reload()
