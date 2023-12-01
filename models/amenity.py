116f960067f14e6a3fe0da96697f5b8851fb7a04
SMCastr <mariesweetycastro@gmail.com>
2023-12-01 10:10:59 -0400

    fix Places _ relationship

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" Amenity Module for HBNB project """
 import models
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
@@ -10,6 +11,9 @@ class Amenity(BaseModel):
     if models.storage_t == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                       backref="amenities",
+                                       viewonly=False)
     else:
         name = ""
 
