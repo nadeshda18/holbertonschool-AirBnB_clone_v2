0ea5ce331c6fa7a0f35e1068edb2710e75c6009d
SMCastr <mariesweetycastro@gmail.com>
2023-12-01 11:03:52 -0400

    fix call imports DB

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ from models.engine.file_storage import FileStorage
 from os import getenv
 from models.engine.db_storage import DBStorage
 
+
 HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
 if HBNB_TYPE_STORAGE == 'db':
