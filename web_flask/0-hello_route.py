3b84149fbc9f4ccda885559e2a691eb558325dec
SMCastr <mariesweetycastro@gmail.com>
2023-12-12 14:05:01 -0400

    modify code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
+"""
+Script that starts a Flask web application
+"""
 
 
 from flask import Flask
 
 
-app = Flask(__name__) # starts flask web app
+# Create a Flask web application instance
+app = Flask(__name__)
 
-
+# Define a route for the root URL ("/") and disable strict_slashes
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Route handler for the root URL ("/") """
+    """
+    Route handler for the root URL ("/")
+    """
     return 'Hello HBNB!'
 
-
+# Check if the script is being run directly
 if __name__ == '__main__':
-    
-    app.run(host='0.0.0.0', port=5000) # runs app at given host and port
+    # Run the Flask web application on 0.0.0.0 (all available network interfaces) on port 5000
+    app.run(host='0.0.0.0', port=5000)
