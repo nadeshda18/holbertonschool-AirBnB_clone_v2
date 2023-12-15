479f543a46e52d04d73ca3054e1cb1fc772bac5f
nadeshda18 <nadeshda02@hotmail.com>
2023-12-15 19:25:22 -0400

    dont know anymore

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,97 +8,9 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """Display hello message
-
-    Returns:
-        str: hello message
-    """
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Display HBNB
-
-    Returns:
-        str: HBNB
-    """
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Display text
-
-    Args:
-        text (str): text to display
-
-    Returns:
-        str: text
-    """
-    return "C {}".format(text.replace("_", " "))
-
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """Display other text
-
-    Args:
-        text (str, optional): Other text to display. Defaults to "is cool".
-
-    Returns:
-        str: text
-    """
-    return "Python {}".format(text.replace("_", " "))
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def number(n):
-    """Display number
-
-    Args:
-        n (int): number to display
-
-    Returns:
-        str: text with number
-    """
-    return "{} is a number".format(n)
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
-    """Display HTML template
-
-    Args:
-        n (int): number to check
-
-    Returns:
-        str: HTML template
-    """
-    return render_template("5-number.html", n=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def number_odd_or_even(n):
-    """Check if number is odd or even
-
-    Args:
-        n (int): number to check
-
-    Returns:
-        str: HTML template
-    """
-    return render_template("6-number_odd_or_even.html", n=n)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    states = storage.all('State').values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
 
 
 @app.route('/cities_by_states')
@@ -109,11 +21,6 @@ def cities_by_states():
     for state in states:
         state.cities = sorted(state.cities, key=lambda city: city.name)
     return render_template('8-cities_by_states.html', states=states)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    storage.close()
 
 
 if __name__ == "__main__":
