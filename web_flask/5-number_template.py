9a8119fa72e060463052a4650285bec8a05619e9
nadeshda18 <nadeshda02@hotmail.com>
2023-12-15 08:09:49 -0400

    lower or upper

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,40 +2,39 @@
 """Starts a Flask web application"""
 from flask import Flask, render_template
 app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
+app.url_map.strict_slashes = False
+@app.route('/')
 def hello():
     """Displays Hello HBNB!"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """Displays HBNB"""
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c(text):
     """Displays C followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python')
+@app.route('/python/<text>')
 def python(text='is cool'):
     """Displays Python followed by the value of the text variable"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number/<int:n>')
 def number(n):
     """Displays n is a number only if n is an integer"""
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>')
 def number_template(n):
     """Displays an HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
