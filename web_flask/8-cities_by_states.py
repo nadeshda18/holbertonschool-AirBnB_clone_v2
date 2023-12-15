a347df4c3e9419b98f6262114670c9ef4e9a376c
nadeshda18 <nadeshda02@hotmail.com>
2023-12-15 08:24:39 -0400

    city

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Start a Flask web app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
