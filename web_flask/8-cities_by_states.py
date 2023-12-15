b33e7fc3fa01705c3c648d6fb0cd011b6483c0e4
nadeshda18 <nadeshda02@hotmail.com>
2023-12-15 14:21:31 -0400

    little tweaks

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""Start a Flask web app"""
-from flask import Flask, render_template
+""" This module starts a Flask web application """""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
+""" Flask class and render_template method"""""
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Display a HTML page"""
+@app.teardown_appcontext
+def teardown(self):
+    storage.close()
+
+
+@app.route('/cities_by_states')
+def cities_by_states():
     states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
