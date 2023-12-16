08653349c8aaff97196a5b2a01bc36c30ff9c1a4
nadeshda18 <nadeshda02@hotmail.com>
2023-12-15 20:18:12 -0400

    leaving it like that

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,11 +8,24 @@ from models.city import City
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
+@app.route('/states_list')
+def states_list():
+    """display html page
+       fetch sorted states to insert into html in UL tag
+    """
     states = storage.all(State).values()
-    states_sorted = sorted(states, key=lambda city: city.name)
-    return render_template('cities_by_states.html', states=states_sorted)
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states')
+def cities_by_states():
+    """display a HTML page with list of all State objects and their cities"""
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    for state in states:
+        state.cities = sorted(state.cities, key=lambda city: city.name)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
